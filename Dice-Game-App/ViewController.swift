10b811168bb03af74e0a71bdfb408153ed327fd4
Dejan Atanasovski <atanasovski_dejan@yahoo.com>
2020-04-22 10:50:04 +0100

    Initial Commit

---
+++ Dice-Game-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice-Game-App
+//
+//  Created by Code Nation on 22/04/2020.
+//  Copyright © 2020 Dejan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
