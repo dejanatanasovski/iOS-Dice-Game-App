028a828527a2fd93a58c4993c4cecf4a41653687
Dejan Atanasovski <atanasovski_dejan@yahoo.com>
2020-04-22 17:27:57 +0100

    Dice-Game-App

--- Dice-Game-App/ViewController.swift
+++ Dice-Game-App/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let array = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    var randomNumber = 0
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var diceImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ButtonPressed(_ sender: Any) {
+        randomNumber = Int(arc4random_uniform(6))
+        diceImg.image = UIImage(named: array[randomNumber])
+        label.text = "Your number is \(randomNumber + 1)"
+    }
 }
 
